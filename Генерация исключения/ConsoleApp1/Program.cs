5c20b678d729f371ba7930acc9474c04f69b7ef9
ulshinvv <158988350+ulshinvv@users.noreply.github.com>
2024-02-12 14:13:36 +0700

    Add files via upload

---
+++ Генерация исключения/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using FunctionCalculator;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = 1;
+            double K = 1;
+            int iterations = 10; 
+            FunctionCalculator1 functionCalculator = new FunctionCalculator1();
+
+            functionCalculator.Calculate(N, iterations,K);
+        }
+    }
+}
---
+++ Генерация исключения/FunctionCalculator/FunctionCalculator1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCalculator
+{
+    public class FunctionCalculator1
+    {
+        public void Calculate(double N, int iterations, double k)
+        {
+            
+            double step = 0.1;
+
+            try
+            {
+                for (double x = 0; x <= k; x += step)
+                {
+                    if (Math.Abs(x - N) < double.Epsilon)
+                    {
+                        throw new ArgumentException("Деление на ноль не допускается. ");
+                    }
+
+                    double result = 1 / (x - N);
+                    Console.WriteLine($"f({x}) = {result}");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Исключение поймано: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"произошла непредвиденная ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Расчет завершен.");
+            }
+        }
+    }
+
+
+}
